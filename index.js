00f3019c31078ed89b0d01c0505cea7bf0dd643f
Becca Jonas <beccajonas3@gmail.com>
2023-10-04 16:15:40 -0400

    modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const parentElement = document.querySelector("body");
+const mainElement = document.getElementById("main");
+parentElement.removeChild(mainElement);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Becca is the champion";
